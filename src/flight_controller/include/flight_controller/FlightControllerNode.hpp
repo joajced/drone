0e8c3ff68e4874e07bed91c4964dc11907ee5cd7
Joaquin Cedron <joajced@proton.me>
2026-08-06 19:24:20 +0200

    Added control loop that publishes motor speeds

--- src/flight_controller/include/flight_controller/FlightControllerNode.hpp
+++ src/flight_controller/include/flight_controller/FlightControllerNode.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "actuator_msgs/msg/actuators.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 
@@ -9,6 +10,11 @@ public:
 
 private:
   void imuCallback(sensor_msgs::msg::Imu::ConstSharedPtr msg);
+  void controlLoop();
 
-  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imuSubscription_;
+  rclcpp::Publisher<actuator_msgs::msg::Actuators>::SharedPtr motorSpeedPublisher_;
+  rclcpp::TimerBase::SharedPtr controlTimer_;
+
+  sensor_msgs::msg::Imu latestImuData_;
 };
--- src/flight_controller/src/FlightControllerNode.cpp
+++ src/flight_controller/src/FlightControllerNode.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <memory>
 
@@ -6,13 +7,38 @@
 FlightControllerNode::FlightControllerNode()
   : Node("flight_controller_node") {
   /* Subscribe to the IMU topic */
-  imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
+  imuSubscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
     "/x500/imu",
     10,
     [this](sensor_msgs::msg::Imu::ConstSharedPtr msg) { imuCallback(msg); }
   );
+
+  /* Publish to the motor speed topic */
+  motorSpeedPublisher_ = this->create_publisher<actuator_msgs::msg::Actuators>(
+    "/x500/command/motor_speed",
+    10
+  );
+
+  /* Initialize timer (250 Hz) */
+  controlTimer_ = this->create_wall_timer(
+    std::chrono::milliseconds(4),
+    [this]() { controlLoop(); }
+  );
+}
+
+void FlightControllerNode::controlLoop() {
+  actuator_msgs::msg::Actuators motorSpeeds;
+
+  motorSpeeds.velocity = {
+    780.0,
+    780.0,
+    780.0,
+    780.0
+  };
+
+  motorSpeedPublisher_->publish(motorSpeeds);
 }
 
 void FlightControllerNode::imuCallback(sensor_msgs::msg::Imu::ConstSharedPtr msg) {
-  std::cout << "Linear Acc Z: " << msg->linear_acceleration.z << " m/s^2" << std::endl;
+  latestImuData_ = *msg;
 }
