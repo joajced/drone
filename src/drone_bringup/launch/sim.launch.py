1a1152168f75f7ef6ae20780731b5e70b68dc250
Joaquin Cedron <joajced@proton.me>
2026-09-15 22:48:04 +0200

    Create launch file

---
+++ src/drone_bringup/launch/sim.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+  # ROS2 <-> Gazebo topic bridges
+
+  imu_bridge = Node(
+    package='ros_gz_bridge',
+    executable='parameter_bridge',
+    arguments=['/x500/imu@sensor_msgs/msg/Imu[gz.msgs.IMU'],
+    output='screen'
+  )
+
+  motor_bridge = Node(
+    package='ros_gz_bridge',
+    executable='parameter_bridge',
+    arguments=['/x500/command/motor_speed@actuator_msgs/msg/Actuators]gz.msgs.Actuators'],
+    output='screen'
+  )
+
+  # Flight controller node
+
+  flight_controller = Node(
+    package='flight_controller',
+    executable='flight_controller_node',
+    output='screen'
+  )
+
+  return LaunchDescription([
+      imu_bridge,
+      motor_bridge,
+      flight_controller
+  ])
